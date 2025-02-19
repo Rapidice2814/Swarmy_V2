1a983861b78c42e1bd67797dd4ae130134ac4d5f
Csongor <c.buzogany@roboteamtwente.nl>
2025-02-19 18:14:17 +0100

    started code for tx IR communication

--- Pringles_V2/Core/Inc/IR_communication.h
+++ Pringles_V2/Core/Inc/IR_communication.h
@@ -10,7 +10,8 @@
 
 #include "main.h"
 
-#define IR_PACKAGE_SIZE 5       //Size of the IR package in bytes
+#define IR_DATA_SIZE 5       //Size of the actual IR data in bytes
+#define IR_PACKET_SIZE (1 + IR_DATA_SIZE + 2 + 1)     //Size of the IR packet in bytes
 #define IR_TX_CHANNEL_COUNT 4   //Number of TX IR channels
 #define IR_RX_CHANNEL_COUNT 8   //Number of RX IR channels
 
@@ -25,10 +26,17 @@ typedef enum{
 }IR_TX_StateTypeDef;
 
 typedef struct{
-    uint8_t buffer[IR_TX_CHANNEL_COUNT][IR_PACKAGE_SIZE];
-    IR_TX_StateTypeDef state;
+    uint8_t buffer[IR_TX_CHANNEL_COUNT][IR_DATA_SIZE];
+    IR_TX_StateTypeDef state[IR_TX_CHANNEL_COUNT];
+    uint8_t byte_counter;
+    uint8_t bit_counter;
 }IR_TX_HandleTypeDef;
 
+
 IR_StatusTypeDef IR_TX_Init(IR_TX_HandleTypeDef *hir_tx);
+IR_StatusTypeDef IR_TX_SetBuffer(IR_TX_HandleTypeDef *hir_tx, uint8_t channel, uint8_t *data);
+IR_StatusTypeDef IR_TX_Start(IR_TX_HandleTypeDef *hir_tx, uint8_t channel);
+IR_StatusTypeDef IR_TX_Stop(IR_TX_HandleTypeDef *hir_tx, uint8_t channel);
+IR_StatusTypeDef IR_TX_Step(IR_TX_HandleTypeDef *hir_tx);
 
 #endif /* INC_IR_COMMUNICATION_H_ */
--- Pringles_V2/Core/Src/IR_communication.c
+++ Pringles_V2/Core/Src/IR_communication.c
@@ -7,15 +7,104 @@
 
 #include "IR_communication.h"
 
+static void increment_tx_counters(IR_TX_HandleTypeDef *hir_tx);
+static uint8_t get_bit(uint8_t byte, uint8_t i);
+static void set_tx_led(uint8_t channel, uint8_t state);
+
 
 IR_StatusTypeDef IR_TX_Init(IR_TX_HandleTypeDef *hir_tx){
-    hir_tx->state = IR_TX_IDLE;
+    for(int i = 0; i < IR_TX_CHANNEL_COUNT; i++){
+        hir_tx->state[i] = IR_TX_IDLE;
+    }
 
     for(int i = 0; i < IR_TX_CHANNEL_COUNT; i++){
-        for(int j = 0; j < IR_PACKAGE_SIZE; j++){
+        for(int j = 0; j < IR_DATA_SIZE; j++){
             hir_tx->buffer[i][j] = 0;
         }
     }
 
+    hir_tx->byte_counter = 0;
+    hir_tx->bit_counter = 0;
+
     return IR_OK;
+}
+
+IR_StatusTypeDef IR_TX_SetBuffer(IR_TX_HandleTypeDef *hir_tx, uint8_t channel, uint8_t *data){
+    if(channel < IR_TX_CHANNEL_COUNT){
+        // for(int i = 0; i < IR_DATA_SIZE; i++){
+        //     hir_tx->buffer[channel][i] = data[i];
+        // }
+        memcpy(hir_tx->buffer[channel], data, IR_DATA_SIZE);
+        return IR_OK;
+    }
+    return IR_ERROR;
+}
+
+IR_StatusTypeDef IR_TX_Start(IR_TX_HandleTypeDef *hir_tx, uint8_t channel){
+    if(channel < IR_TX_CHANNEL_COUNT){
+        hir_tx->state[channel] = IR_TX_SENDING;
+        return IR_OK;
+    }
+    return IR_ERROR;
+}
+
+IR_StatusTypeDef IR_TX_Stop(IR_TX_HandleTypeDef *hir_tx, uint8_t channel){
+    if(channel < IR_TX_CHANNEL_COUNT){
+        hir_tx->state[channel] = IR_TX_IDLE;
+        return IR_OK;
+    }
+    return IR_ERROR;
+}
+
+
+
+IR_StatusTypeDef IR_TX_Step(IR_TX_HandleTypeDef *hir_tx){
+    for(int channel = 0; channel < IR_TX_CHANNEL_COUNT; channel++){
+        if(hir_tx->state[channel] == IR_TX_SENDING){
+            set_tx_led(channel, get_bit(hir_tx->buffer[channel][hir_tx->byte_counter], hir_tx->bit_counter)); 
+        }
+        else{
+            set_tx_led(channel, 0);
+        }
+    }
+    
+    increment_tx_counters(hir_tx);
+
+
+    return IR_OK;
+}
+
+static uint8_t get_bit(uint8_t byte, uint8_t i) {
+    if (i < 8) {
+        return (byte >> i) & 0x01;
+    }
+    return 0;
+}
+
+static void increment_tx_counters(IR_TX_HandleTypeDef *hir_tx){
+    hir_tx->bit_counter++;
+    if(hir_tx->bit_counter >= 8){
+        hir_tx->bit_counter = 0;
+        hir_tx->byte_counter++;
+        if(hir_tx->byte_counter >= IR_PACKET_SIZE){
+            hir_tx->byte_counter = 0;
+        }
+    }
+}
+
+static void set_tx_led(uint8_t channel, uint8_t state){
+    switch(channel){
+        case 0:
+            HAL_GPIO_WritePin(IR1_GPIO_Port, IR1_Pin, state);
+            break;
+        case 1:
+            HAL_GPIO_WritePin(IR2_GPIO_Port, IR2_Pin, state);
+            break;
+        case 2:
+            HAL_GPIO_WritePin(IR3_GPIO_Port, IR3_Pin, state);
+            break;
+        case 3:
+            HAL_GPIO_WritePin(IR4_GPIO_Port, IR4_Pin, state);
+            break;
+    }
 }
